5814b0483bdac194d17210f676247f94e3d84f19
nolayercode <vyelhekar@gmail.com>
2019-12-13 01:48:20 +0530

    teaking input from textarea and displaying over ui and routing working fine

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PostsComponent } from './posts/posts.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
 
 const routes: Routes = [];
 
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Alert } from 'selenium-webdriver';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+
+export class PostCreateComponent implements OnInit {
+
+  enteredValue = '';
+  tesOutput = 'hello';
+  constructor() {
+    // this.tesOutput = 'hello';
+   }
+
+  ngOnInit() {
+  }
+
+  onClick() {
+    // console.dir(testInput);
+    this.tesOutput = this.enteredValue;
+  }
+}
